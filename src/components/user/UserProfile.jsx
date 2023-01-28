a1bc4889b49bcaae2f2e3ca9685af1310f01a884
puneethkumar-v <pk.puni872@gmail.com>
2023-01-28 22:00:52 +0530

    Added Sidebar :sparkle:

--- src/components/user/UserProfile.jsx
+++ src/components/user/UserProfile.jsx
@@ -1,24 +1,30 @@
-import React,{useState} from 'react'
-import { useDispatch } from 'react-redux'
-import Cookies from 'js-cookie'
-import { stateModifier } from '../../features/reducers/slice'
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import Cookies from "js-cookie";
+import { stateModifier } from "../../features/reducers/slice";
+import Sidebar from "../Sidebar/Sidebar";
 
 function UserProfile() {
-    const dispatch = useDispatch()
-   
+  const dispatch = useDispatch();
 
-const handleLogout=()=>{
-    Cookies.remove("Token")
-    dispatch(stateModifier(false))
-}
+  const handleLogout = () => {
+    Cookies.remove("Token");
+    dispatch(stateModifier(false));
+  };
 
   return (
-    <div>
-      <h1>Hello</h1>
-      <button className='btn btn-error btn-outline' onClick={handleLogout}>Logout</button>
+    <div style={{ display: "flex" }}>
+      <div>
+        <Sidebar />
+      </div>
+      <div>
+        <h1>Hello</h1>
+        <button className="btn btn-error btn-outline" onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default UserProfile
+export default UserProfile;

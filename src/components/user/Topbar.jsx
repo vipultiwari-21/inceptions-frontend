889050498919ddce074a7464b17f9c530e397ef0
puneethkumar-v <pk.puni872@gmail.com>
2023-01-31 22:54:06 +0530

    Added Topbar component

---
+++ src/components/user/Topbar.jsx
@@ -0,0 +1,44 @@
+import { Box, IconButton, Typography, useTheme, Button } from "@mui/material";
+import InputBase from "@mui/material/InputBase";
+import LogoutIcon from "@mui/icons-material/Logout";
+import SearchIcon from "@mui/icons-material/Search";
+import { useDispatch } from "react-redux";
+import Cookies from "js-cookie";
+import { stateModifier } from "../../features/reducers/slice";
+
+const Topbar = () => {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    Cookies.remove("Token");
+    dispatch(stateModifier(false));
+  };
+  return (
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      width="100%"
+      backgroundColor="white"
+      p={2}
+    >
+      {/* SEARCH BAR */}
+      <Box display="flex" borderRadius="3px">
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      {/* ICONS */}
+      <Box display="flex">
+        <>
+          <IconButton onClick={handleLogout}>
+            <LogoutIcon />
+          </IconButton>
+        </>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;

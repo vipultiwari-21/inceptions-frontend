c8df48b3849f4ca36b0b9d127121112100d3f0f0
puneethkumar-v <pk.puni872@gmail.com>
2023-02-05 19:19:08 +0530

    Added Header component

---
+++ src/components/Sidebar/Header.jsx
@@ -0,0 +1,22 @@
+import { Typography, Box, useTheme } from "@mui/material";
+
+const Header = ({ title, subtitle }) => {
+  const theme = useTheme();
+  return (
+    <Box mb="30px">
+      <Typography
+        variant="h3"
+        // color={colors.grey[100]}
+        fontWeight="bold"
+        sx={{ m: "0 0 5px 0" }}
+      >
+        {title}
+      </Typography>
+      <Typography variant="h5" /* color={colors.greenAccent[400]} */>
+        {subtitle}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Header;

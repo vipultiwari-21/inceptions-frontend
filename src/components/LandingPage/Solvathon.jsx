33363994695e2695b3bf3627ea678e3d15ce4966
i-punithgowda <impuni007@gmail.com>
2023-02-14 13:00:37 +0530

    added new event details modified detailed event details

--- src/components/LandingPage/Solvathon.jsx
+++ src/components/LandingPage/Solvathon.jsx
@@ -1,17 +1,74 @@
-import { Box } from '@mui/system'
-import React from 'react'
-import 'animate.css';
+import { Box } from "@mui/system";
+import React from "react";
+import { Link } from "react-router-dom";
+import solveathonImage from "../../assets/svg/solveathon.svg";
+import TrophyImage from "../../assets/images/trophy.png";
+import './textAnimation.css'
+
 
 function Solvathon() {
   return (
-    <Box className="flex flex-center items-center">
-    <h1 class="animate__animated  animate__shakeX">20000</h1>
+    <Box className="flex flex-center items-center w-full ">
+      <div
+        className={`event-box p-5`}
+        style={{
+          justifyContent: "space-around",
+          alignItems: "center",
+          margin: "2rem auto",
+          textAlign: "center",
+        }}
+      >
+        <div className="lg:w-1/2 sm:w-3/4 ">
+          <h1
+            className={`text-3xl my-5 text-primary  text-center lg:text-left `}
+          >
+            SOLVEATHON
+          </h1>
+          <div>
+            <p
+              className="text-xl text-neutral-content text-justify lg:text-justify"
+              style={{ lineHeight: "1.5" }}
+            >
+              Solve-a-thon is a solution event focused on building an AI/ML and
+              vision based android application, where enrolled teams will get to
+              solve real time problems that could be planet changing tech. It is
+              an opportunity for students to showcase their skills, learn new
+              technologies and potentially develop a project into a product.
+            </p>
+
+            <div>
+            <h3 className="text-center my-8 text-3xl text-primary font-bold ">Price details </h3>
+
+          <div className="text-lg lg:text-2xl horizontal-shake font-bold text-warning text-price flex-col   flex w-full items-center justify-around">
+          <span>1st Price : 15000 </span>
+          <span>2nd Price : 10000 </span>
+          <span>3rd Price : 5000 </span>
+
+          </div>
+            </div>
+
+            <div className="trophy py-8 flex flex-col lg:flex-row items-center justify-evenly w-full">
+              <div className="">
+                <img src={TrophyImage} alt="price-img" />
+              </div>
+
+            
+            </div>
+          </div>
+        </div>
+        <div className="">
+          <img
+            src={solveathonImage}
+            alt="solvathon-logo"
+            style={{ width: "25rem" }}
+          />
+          <Link to="/details/solveathon" className={`btn btn-primary btn-wide my-8 `}>
+            Details
+          </Link>
+        </div>
+      </div>
     </Box>
-  )
+  );
 }
 
-export default Solvathon
-
-
-
-
+export default Solvathon;

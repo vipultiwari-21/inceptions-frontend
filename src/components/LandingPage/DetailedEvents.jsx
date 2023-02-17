da5eafde2329553bd702a9b62ace3cd283e900ce
puneethkumar-v <pk.puni872@gmail.com>
2023-02-17 12:30:07 +0530

    Detailed events done

--- src/components/LandingPage/DetailedEvents.jsx
+++ src/components/LandingPage/DetailedEvents.jsx
@@ -14,18 +14,17 @@ const DetailedEvents = () => {
       <Background />
 
       <Box className="bg-neutral  lg:w-10/12 lg:h-5/6 w-full h-full px-3 lg:p-0  flex lg:flex-row flex-col">
-      {EventDetailList.map((event) => {
-        if (event.route == id) {
-          
-          return (
-            <img
-          src={event.img}
-          alt="Event Image"
-          className="h-full w-5/12 hidden lg:block"
-        />
-          )
-        }})
-      }
+        {EventDetailList.map((event) => {
+          if (event.route == id) {
+            return (
+              <img
+                src={event.img}
+                alt="Event Image"
+                className="h-full w-5/12 hidden lg:block"
+              />
+            );
+          }
+        })}
 
         <Box className="h-full lg:w-7/12 ">
           <EventNavbar />
@@ -74,17 +73,20 @@ const DetailedEvents = () => {
                       <p className="my-8">
                         <span className="text-xl text-secondary">Contact</span>
 
-                      <div className="flex  flex-col justify-evenly items-center w-full lg:flex-row ">
-                      { event.contact.map((contacts)=>{
-                        return <ul className="my-3">
-                            <li className="text-warning">{contacts.type}</li>
-                          <li>{contacts.name}</li>
-                          <li>{contacts.phone}</li>
-                          <li>{contacts.email}</li>
-                        </ul>
-                       })}
-                      </div>
-                       
+                        <div className="flex  flex-col justify-evenly items-center w-full lg:flex-row ">
+                          {event.contact.map((contacts) => {
+                            return (
+                              <ul className="my-3">
+                                <li className="text-warning">
+                                  {contacts.type}
+                                </li>
+                                <li>{contacts.name}</li>
+                                <li>{contacts.phone}</li>
+                                <li>{contacts.email}</li>
+                              </ul>
+                            );
+                          })}
+                        </div>
                       </p>
                     ) : null}
                   </Box>

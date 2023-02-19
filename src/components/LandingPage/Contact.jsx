a7f7aaa74e0500040d4e83951d26e38d6a9326fb
i-punithgowda <impuni007@gmail.com>
2023-02-19 09:40:30 +0530

    added faculty co ordinators

--- src/components/LandingPage/Contact.jsx
+++ src/components/LandingPage/Contact.jsx
@@ -2,19 +2,21 @@ import { Container } from "@mui/material";
 import React from "react";
 import MaleAstronaut from "../../assets/svg/maleAstro.svg";
 import FemaleAstronaut from "../../assets/svg/female.svg";
-import Faculty1 from '../../assets/svg/faculty1.svg'
-import Faculty2 from '../../assets/svg/faculty2.svg'
-import Grid from '@mui/material/Grid';
+import Faculty1 from "../../assets/svg/faculty1.svg";
+import Faculty2 from "../../assets/svg/faculty2.svg";
+import Faculty3 from "../../assets/svg/faculty3.svg";
+import Grid from "@mui/material/Grid";
 import { Box } from "@mui/material";
-import EmailIcon from '@mui/icons-material/Email';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
-
-
+import EmailIcon from "@mui/icons-material/Email";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 
 function Contact() {
   return (
     <Container maxWidth="xl mt-16 text-neutral-content " id="contact">
-      <h1 className="sub-heading text-secondary text-bold text-3xl " id="contact">
+      <h1
+        className="sub-heading text-secondary text-bold text-3xl "
+        id="contact"
+      >
         CONTACT US
       </h1>
 
@@ -23,26 +25,27 @@ function Contact() {
       </h4>
 
       <Grid container className="lg:px-20" spacing={6}>
-        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}  >
+        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
           <Box
             display="flex"
             justifyContent="center"
             alignItems="center"
-            flexDirection="column"    >
+            flexDirection="column"
+          >
             <img src={MaleAstronaut} alt="male" className="img-contact" />
             <h3 className="text-center  text-xl text-bold">NITHIN</h3>
             <h4 className="text-center my-1 text-xl text-bold flex ">
-            <EmailIcon className="mr-3" style={{color:'#E6E6E6  '}} />
-            nithin.mca21@rvce.edu.in
+              <EmailIcon className="mr-3" style={{ color: "#E6E6E6  " }} />
+              nithin.mca21@rvce.edu.in
             </h4>
-            
             <h4 className="text-center text-xl text-bold">
-            <LocalPhoneIcon className="mr-3" style={{color:'#E6E6E6 '}} />
-            +91-8310193605
-            </h4>          </Box>
+              <LocalPhoneIcon className="mr-3" style={{ color: "#E6E6E6 " }} />
+              +91-8310193605
+            </h4>{" "}
+          </Box>
         </Grid>
 
-        <Grid item xs={12} sm={12} md={6} lg={6} xl={6} >
+        <Grid item xs={12} sm={12} md={6} lg={4} xl={6}>
           <Box
             display="flex"
             justifyContent="center"
@@ -50,69 +53,82 @@ function Contact() {
             flexDirection="column"
           >
             <img src={FemaleAstronaut} alt="male" className="img-contact" />
-            <h3 className="text-center  text-xl text-bold">
-              SANJANA AVADHANI
-            </h3>
+            <h3 className="text-center  text-xl text-bold">SANJANA AVADHANI</h3>
             <h4 className="text-center my-1 text-xl text-bold flex ">
-            <EmailIcon className="mr-3" style={{color:'#FFEEBE'}} />
-            sanjanaga.mca21@rvce.edu.in
+              <EmailIcon className="mr-3" style={{ color: "#FFEEBE" }} />
+              sanjanaga.mca21@rvce.edu.in
             </h4>
             <h4 className="text-center text-xl text-bold">
-            <LocalPhoneIcon className="mr-3" style={{color:'#FFEEBE'}} />
-            +91 99010 91362
+              <LocalPhoneIcon className="mr-3" style={{ color: "#FFEEBE" }} />
+              +91 99010 91362
             </h4>
           </Box>
         </Grid>
       </Grid>
 
-
-
-
       {/* Faculty co ordinators */}
 
       <h4 className="text-center my-16 text-2xl text-bold ">
-      Faculty Co-ordinators
-    </h4>
-
+        Faculty Co-ordinators
+      </h4>
 
       <Grid container className="lg:px-20 " spacing={6}>
-      <Grid item xs={12} sm={12} md={6} lg={6} xl={6}  >
-        <Box
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          flexDirection="column"    >
-          <img src={Faculty1} alt="male" className="img-contact" />
+        <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <img src={Faculty1} alt="male" className="img-contact" />
 
-          <h3 className="text-center  text-xl text-bold">PROF. SAVITA SHEELAVANT</h3>
-          <h4 className="text-center my-1 text-xl text-bold flex ">
-          <EmailIcon className="mr-3" style={{color:'#E6E6E6  '}} />
-          savitas.sheelavant@rvce.edu.in
-          </h4>
-          
-                 </Box>
-      </Grid>
-
-      <Grid item xs={12} sm={12} md={6} lg={6} xl={6} >
-        <Box
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          flexDirection="column"
-        >
-        <img src={Faculty2} alt="male" className="img-contact" />
-
-           <h3 className="text-center  text-xl text-bold">
-           Dr. SAVITHA R
-          </h3>
-          <h4 className="text-center my-1 text-xl text-bold flex ">
-          <EmailIcon className="mr-3" style={{color:'#FFEEBE'}} />
-          savithar@rvce.edu.in
-          </h4>
+            <h3 className="text-center  text-xl text-bold">
+              PROF. SAVITA SHEELAVANT
+            </h3>
+            <h4 className="text-center my-1 text-xl text-bold flex ">
+              <EmailIcon className="mr-3" style={{ color: "#E6E6E6  " }} />
+              savitas.sheelavant@rvce.edu.in
+            </h4>
           </Box>
-      </Grid>
-    </Grid>
+        </Grid>
 
+        <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <img src={Faculty3} alt="male" className="img-contact" />
+
+            <h3 className="text-center  text-xl text-bold">
+              {" "}
+              Dr. Andhe Dharani
+            </h3>
+            <h4 className="text-center my-1 text-xl text-bold flex ">
+              <EmailIcon className="mr-3" style={{ color: "#FFEEBE" }} />
+              andhedharani@rvce.edu.in
+            </h4>
+          </Box>
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <img src={Faculty2} alt="male" className="img-contact" />
+
+            <h3 className="text-center  text-xl text-bold">Dr. SAVITHA R</h3>
+            <h4 className="text-center my-1 text-xl text-bold flex ">
+              <EmailIcon className="mr-3" style={{ color: "#FFEEBE" }} />
+              savithar@rvce.edu.in
+            </h4>
+          </Box>
+        </Grid>
+      </Grid>
     </Container>
   );
 }

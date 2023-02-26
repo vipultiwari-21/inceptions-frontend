8437b881ee5d926b9eb6244d36cf9e79af1edae6
puneethkumar-v <pk.puni872@gmail.com>
2023-02-26 13:56:01 +0530

    Changed the kwality walls logo

--- src/components/LandingPage/Sponsorship.jsx
+++ src/components/LandingPage/Sponsorship.jsx
@@ -8,11 +8,11 @@ import Grid from "@mui/material/Grid";
 import { Box } from "@mui/material";
 import DeerajPropertyLogo from "../../assets/Sponsors/DeerajProperty.png";
 import PepsiLogo from "../../assets/Sponsors/pepsi.svg";
-import KWLogo from "../../assets/Sponsors/kw.png";
+import KWLogo from "../../assets/Sponsors/kw.svg";
 import VarunBeweregesLogo from "../../assets/Sponsors/VB.svg";
 import PrachiDentalClinicLogo from "../../assets/Sponsors/prachi.svg";
 import SriLakshmiConstructor from "../../assets/Sponsors/srilakshmi.svg";
-import KwalityWalls from "../../assets/Sponsors/kwalityWalls.png";
+import KwalityWalls from "../../assets/Sponsors/kw.svg";
 
 function Sponsorship() {
   return (
